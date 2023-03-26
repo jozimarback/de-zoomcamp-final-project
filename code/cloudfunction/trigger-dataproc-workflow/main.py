a86106ae05a95774bf24c917bda8d40e52481c0a
Jozimar Back <jozimar5@gmail.com>
2023-03-26 18:48:13 -0300

    cloud function start dataproc

---
+++ code/cloudfunction/trigger-dataproc-workflow/main.py
@@ -0,0 +1,53 @@
+from google.cloud import dataproc
+from google.api_core.client_options import ClientOptions
+from datetime import datetime
+from time import sleep
+import logging
+import os
+
+_REGION = os.environ.get('REGION')
+_PROJECT_ID = os.environ.get('PROJECT_ID')
+_WORKFLOW_TEMPLATE = os.environ.get('WORKFOW_TEMPLATE')
+
+def _check_execution(execution):
+    """Check dataproc workflow execution
+
+    Args:
+        execution : Dataproc execution object
+
+    Raises:
+        Exception: Except in case job isn't running
+    """
+
+    # Aguarda o início da execução do primeiro job
+    job_running = False
+    state_running = 3
+    while execution.running() and not job_running:
+        job_running = any([job.state == state_running for job in execution.metadata.graph.nodes])
+        sleep(2)
+
+    if not job_running:
+        raise Exception(execution.exception())
+
+def main(request):
+    """_summary_
+
+    Args:
+        request (http.request): Request to cloud function
+    """
+
+    start = datetime.utcnow()
+    options = ClientOptions(api_endpoint = f"{_REGION}-dataproc.googleapis.com:443")
+    client = dataproc.WorkflowTemplateServiceClient(client_options = options)
+    name = client.workflow_template_path(_PROJECT_ID, _REGION, _WORKFLOW_TEMPLATE)
+
+    
+    logging.info(f"Starting workflow {_WORKFLOW_TEMPLATE}")
+    execution = client.instantiate_workflow_template(
+        name = name,
+        parameters = {}
+    )
+    _check_execution(execution)
+
+    
+    logging.info(f'Workflow {_WORKFLOW_TEMPLATE} started ({datetime.utcnow() - start})')

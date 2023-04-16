01d84d4948c65070979814b44fb19a43724f915e
Jozimar Back <jozimar5@gmail.com>
2023-04-16 18:41:21 -0300

    cloud function execute dataproc workflow

--- terraform/cloud-functions.tf
+++ terraform/cloud-functions.tf
@@ -1,25 +1,26 @@
-# data "archive_file" "trigger-dataproc-workflow-code-zip" {
-#     type = "zip"
-#     source_dir = "../code/cloudfunction/trigger-dataproc-workflow"
-#     output_path = "cloudfunction/trigger-dataproc-workflow"
-# }
+data "archive_file" "trigger-dataproc-workflow-code-zip" {
+    type = "zip"
+    source_dir = "../code/cloudfunction/trigger-dataproc-workflow"
+    output_path = "cloudfunction/trigger-dataproc-workflow"
+}
 
-# resource "google_storage_bucket_object" "trigger-dataproc-workflow-code" {
-#     name = "${data.archive_file.trigger-dataproc-workflow-code.name}-${data.archive_file.trigger-dataproc-workflow-code.output_sha}.zip"
-#     buket = google_storage_bucket.code.name
-#     source = data.archive_file.trigger-dataproc-workflow-code-zip.output_path
-# }
+resource "google_storage_bucket_object" "trigger-dataproc-workflow-code" {
+    name = "${data.archive_file.trigger-dataproc-workflow-code-zip.name}-${data.archive_file.trigger-dataproc-workflow-code-zip.output_sha}.zip"
+    buket = google_storage_bucket.code.name
+    source = data.archive_file.trigger-dataproc-workflow-code-zip.output_path
+}
 
-# resource "google_cloudfunctions_function" "trigger-dataproc-workflow" {
-#   name = "${var.data-project}-trigger-dataproc-workflow"
-#   runtime = "python38"
-#   entry_point = "main"
-#   trigger_http = true
-#   available_memory_mb = 256
-#   source_archive_bucket = google_storage_bucket.code.name
-#   source_archive_object = google_storage_bucket_object.trigger-dataproc-workflow-code.name
-#   environment_variables = {
-#     "REGION" = var.region
-#     # "DATAPROCWORKFLOW" = google_dataproc_workflow_template.dataproc_average_years_schooling.name
-#   }
-# }
+resource "google_cloudfunctions_function" "trigger-dataproc-workflow" {
+  name = "${var.data-project}-trigger-dataproc-workflow"
+  runtime = "python38"
+  entry_point = "main"
+  trigger_http = true
+  available_memory_mb = 256
+  source_archive_bucket = google_storage_bucket.code.name
+  source_archive_object = google_storage_bucket_object.trigger-dataproc-workflow-code.name
+  environment_variables = {
+    "REGION" = var.region
+    "PROJECT_ID" = var.project
+    "DATAPROC_WORKFLOW" = google_dataproc_workflow_template.dataproc_average_years_schooling.name
+  }
+}

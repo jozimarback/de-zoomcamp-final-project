0c24c32a898531dd9cbe46e07d7070cb9a8a3dd5
Jozimar Back <jozimar5@gmail.com>
2023-04-16 17:23:13 -0300

    add bucket raw

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -20,3 +20,12 @@ resource "google_storage_bucket" "tmp-dataproc" {
     }
   }
 }
+
+resource "google_storage_bucket" "raw" {
+  project = var.project
+  name = "${var.data-project}-raw"
+  force_destroy = false
+  uniform_bucket_level_access = true
+  location = var.region
+  labels = local.labels
+}

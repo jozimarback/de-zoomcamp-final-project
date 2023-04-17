42bffb4243d0a592ddc3e231c23e8c0400deca30
Jozimar Back <jozimar5@gmail.com>
2023-04-17 18:01:19 -0300

    doc string

--- code/dataproc/extract/average-years-schooling-extract.py
+++ code/dataproc/extract/average-years-schooling-extract.py
@@ -10,17 +10,27 @@ import logging
 
 __BUCKET_RAW = sys.argv[1]
 
-def extract_kaggle_dataset(file):  
+def extract_kaggle_dataset(file_name:str)->None:
+    """Download kaggle dataset
+
+    Args:
+        file_name (str): Filename of the dataset
+    """
     api = KaggleApi()
     api.authenticate()
     api.dataset_download_file("fredericksalazar/average-years-of-schooling-since-1870-2017",
-                                file)
+                                file_name)
 
-def send_file_to_raw_bucket(file):
+def send_file_to_raw_bucket(file_name:str)->None:
+    """Send file to raw bucket GCP
+
+    Args:
+        file_name (str): File name to send to
+    """
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(__BUCKET_RAW)
-    blob = bucket.blob(f'csv/{file}')
-    blob.upload_from_filename(file)
+    blob = bucket.blob(f'csv/{file_name}')
+    blob.upload_from_filename(file_name)
 
 if __name__ == "__main__":
     spark = (
@@ -29,8 +39,8 @@ if __name__ == "__main__":
         .getOrCreate()
     )
 
-    file = "mean-years-of-schooling-long-run.csv"
+    file_name = "mean-years-of-schooling-long-run.csv"
     if not os.path.isfile('/root/.kaggle/kaggle.json'):
         logging.error("File kaggle.json does not exist")
-    extract_kaggle_dataset(file)
-    send_file_to_raw_bucket(file)
+    extract_kaggle_dataset(file_name)
+    send_file_to_raw_bucket(file_name)

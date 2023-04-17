17aff5aa291ecdae487913313350dcdfa9d81159
Jozimar Back <jozimar5@gmail.com>
2023-04-17 09:46:07 -0300

    kaggle file api

--- code/dataproc/extract/average-years-schooling-extract.py
+++ code/dataproc/extract/average-years-schooling-extract.py
@@ -1,7 +1,6 @@
 from pyspark.sql import SparkSession, functions as f
 from pyspark.sql.types import *
 from google.cloud import storage
-from kaggle.api.kaggle_api_extend import KaggleApi
 
 from requests.adapters import HTTPAdapter, Retry
 import requests
@@ -14,6 +13,26 @@ __BUCKET_RAW = sys.argv[0]
 os.environ["KAGGLE_USERNAME"] = sys.argv[1]
 os.environ["KAGGLE_KEY"] = sys.argv[2]
 
+def create_kaggle_auth_file():
+    kaggle_json_dir = '/root/.kaggle'
+    if not os.path.exists(kaggle_json_dir):
+        os.makedirs(kaggle_json_dir) 
+    with open(f'{kaggle_json_dir}/kaggle.json', 'w') as f:
+        json.dump({"username":sys.argv[1], "key":sys.argv[2]}, f)
+    os.chmod(f'{kaggle_json_dir}/kaggle.json', 600)
+
+def extract_kaggle_dataset(file):    
+    api = KaggleApi()
+    api.authenticate()
+    api.dataset_download_file("fredericksalazar/average-years-of-schooling-since-1870-2017",
+                                file)
+
+def send_file_to_raw_bucket(file):
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(__BUCKET_RAW)
+    blob = bucket.blob(f'csv/{file}')
+    blob.upload_from_filename(file)
+
 if __name__ == "__main__":
     spark = (
         SparkSession.builder
@@ -21,20 +40,9 @@ if __name__ == "__main__":
         .getOrCreate()
     )
 
-    kaggle_json_dir = '/root/.kaggle/'
-    if not os.path.exists(kaggle_json_dir):
-        os.makedirs(kaggle_json_dir) 
-    with open(f'{kaggle_json_dir}kaggle.json', 'w') as f:
-        f.write(json.dumps({"username":sys.argv[1],"key":sys.argv[2]}))
-    os.chmod(f'{kaggle_json_dir}kaggle.json', 600)  
-
-    api = KaggleApi()
-    api.authenticate()
+    create_kaggle_auth_file()     
     file = "mean-years-of-schooling-long-run.csv"
-    api.dataset_download_file("fredericksalazar/average-years-of-schooling-since-1870-2017",
-                                file)
-
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(__BUCKET_RAW)
-    blob = bucket.blob(f'csv/{file}')
-    blob.upload_from_filename(file)
+    
+    from kaggle.api.kaggle_api_extend import KaggleApi
+    extract_kaggle_dataset(file)
+    send_file_to_raw_bucket(file)

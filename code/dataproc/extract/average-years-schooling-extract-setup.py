2846cd80811abb834eb800652e57f64b4db8a03a
Jozimar Back <jozimar5@gmail.com>
2023-04-17 14:02:32 -0300

    creation kaggle file

--- code/dataproc/extract/average-years-schooling-extract-setup.py
+++ code/dataproc/extract/average-years-schooling-extract-setup.py
@@ -6,12 +6,17 @@ from pathlib import Path
 import sys
 
 def create_kaggle_auth_file():
-    kaggle_json_dir = Path('/opt/kaggle')
+    kaggle_json_dir = '/root/.kaggle'
     os.makedirs(kaggle_json_dir, exist_ok=True)
     with open(f'{kaggle_json_dir}/kaggle.json', 'w') as file:
         json.dump({'username':sys.argv[0],'key':sys.argv[1]}, file)
     os.chmod(f'{kaggle_json_dir}/kaggle.json', 600)
-    logging.info('Created kaggle file')
+   
+    if not os.path.isfile('/root/.kaggle/kaggle.json'):
+        logging.error("File kaggle.json does not exist")
+    else:
+        print('Created kaggle file')
+        logging.info('Created kaggle file')
 
 if __name__ == "__main__":
     spark = (
